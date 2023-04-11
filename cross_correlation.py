1b8895824ee3a7b347bb72d5c3d0afca9874d06a
harshcht <chittoraharsh98@gmail.com>
2023-04-11 17:31:45 +0530

    cros correlation in nci and dam

--- cross_correlation.py
+++ cross_correlation.py
@@ -1,18 +1,30 @@
 import numpy as np
+import csv
+from ast import literal_eval
 
+nci=[]
+dam_prices=[]
+with open('nci_data.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in reader:
+        #print(', '.join(row))
+        nci.append(literal_eval(row[0]))
+        dam_prices.append(literal_eval(row[1]))
 # Define two time series
-x = np.array([1, 2, 3, 4, 5])
-y = np.array([2, 4, 6, 8, 10])
-
+#x = np.array([1, 2, 3, 4, 5])
+#y = np.array([-10, 1, 2, 3, 4])
+#print(nci)
+#print(np.array(nci))
 # Compute the cross-correlation using the numpy.correlate function
-corr = np.correlate(x, y, mode='full')
-
+corr = np.correlate(np.array(nci), np.array(dam_prices), mode='full')
+print(len(nci))
+print(len(dam_prices))
 # Compute the lags for the cross-correlation
-lags = np.arange(-len(x)+1, len(y))
+lags = np.arange(-len(nci)+1, len(dam_prices))
 
 # Plot the cross-correlation function
 import matplotlib.pyplot as plt
-plt.stem(lags, corr)
+plt.plot(lags, corr)
 plt.xlabel('Lag')
 plt.ylabel('Cross-correlation')
 plt.show()
